c9425d4bf8c70f18affa557be27403b800bdbba1
anandgandharwa-create <anandgandharwa@gmail.com>
2026-02-24 16:34:04 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -32,3 +32,14 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
